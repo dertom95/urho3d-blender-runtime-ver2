b64be711b683e77fa5859c7fcb1efb7622a74cf5
Thomas Trocha <thomas.trocha@gmail.com>
2020-01-11 15:28:10 +0100

    modified dockerized.sh to call docker as sudo

--- script/dockerized.sh
+++ script/dockerized.sh
@@ -24,28 +24,27 @@
 if [[ $# -eq 0 ]]; then echo "Usage: dockerized.sh native|mingw|android|web not:rpi|arm [command [params]]"; exit 1; fi
 
 PROJECT_DIR=$(cd "${0%/*}/.." || exit 1; pwd)
-
+echo 1
 if [[ ! $DBE_TAG ]]; then
     # If the command failed or not on a tag then use 'master' by default; TRAVIS_COMMIT should be empty for non-CI usage
     DBE_TAG=$(git describe --exact-match "$TRAVIS_COMMIT" 2>/dev/null || echo master)
 fi
-
+echo 2
 BuildEnvironment=-$1; shift
 BuildEnvironment=${BuildEnvironment/-base}
-
-# Workaround Travis-CI intermittent network I/O error
-if [[ $TRAVIS ]]; then while (! docker pull "urho3d/dockerized$BuildEnvironment:$DBE_TAG"); do sleep 10; done; fi
+echo 3
 
 # shellcheck disable=SC1083
-if [[ $(docker version -f {{.Client.Version}}) =~ ^([0-9]+)\.0*([0-9]+)\. ]] && (( BASH_REMATCH[1] * 100 + BASH_REMATCH[2] >= 1809 )); then
-echo     docker run -it --rm -h fishtank \
+if [[ $(sudo docker version -f {{.Client.Version}}) =~ ^([0-9]+)\.0*([0-9]+)\. ]] && (( BASH_REMATCH[1] * 100 + BASH_REMATCH[2] >= 1809 )); then
+echo 4
+	echo     sudo docker run -it --rm -h fishtank \
         -e HOST_UID="$(id -u)" -e HOST_GID="$(id -g)" -e PROJECT_DIR="$PROJECT_DIR" $NEW_URHO3D_HOME \
         --mount type=bind,source="$PROJECT_DIR",target="$PROJECT_DIR" \
         -v $PROJECT_DIR/android/launcher-app:/Urho3D/android/launcher-app \
         --name "dockerized$BuildEnvironment" \
         "dertom95/urho3d$BuildEnvironment:$DBE_TAG" "$@"
-
-    	docker run -it --rm -h fishtank \
+echo 5
+    	sudo docker run -it --rm -h fishtank \
         -e HOST_UID="$(id -u)" -e HOST_GID="$(id -g)" -e PROJECT_DIR="$PROJECT_DIR" $NEW_URHO3D_HOME \
         --mount type=bind,source="$PROJECT_DIR",target="$PROJECT_DIR" \
         -v $PROJECT_DIR/android/launcher-app:/Urho3D/android/launcher-app \
@@ -55,7 +54,7 @@ else
     echo "CAUTION: UNTESTED"
     echo "CAUTION: UNTESTED"
     # Fallback workaround on older Docker CLI version
-    docker run -it --rm -h fishtank \
+    sudo docker run -it --rm -h fishtank \
         -e HOST_UID="$(id -u)" -e HOST_GID="$(id -g)" -e PROJECT_DIR="$PROJECT_DIR" \
         --mount type=bind,source="$PROJECT_DIR",target="$PROJECT_DIR" \
         -v $PROJECT_DIR/android/launcher-app:/Urho3D/android/launcher-app \
